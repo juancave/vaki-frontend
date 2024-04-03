bcc195ffb72315fe715a28ffff099015c6bed052
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-04-03 11:25:01 -0500

    Initial vite project plus tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="h-screen w-full flex items-center justify-center font-medium">
+        App works!
+      </p>
+    </>
+  )
+}
+
+export default App
