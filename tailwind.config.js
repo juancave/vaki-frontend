0c37ac1ce2c9e97d59de8a9c6c7bf731199734bb
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-04-03 13:33:34 -0500

    Adding custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vaki-green': '#66B04C',
+        'vaki-red': '#FF2530',
+        'vaki-black': '#3A3A3A',
+        'vaki-primary': '#36190D',
+        'vaki-secondary': '#FFA72F',
+      },
+    },
   },
   plugins: [],
 };
