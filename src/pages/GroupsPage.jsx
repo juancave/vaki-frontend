960d047645f72bc32df5e8eb0b4b4794f7f8fbb9
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-04-03 17:56:20 -0500

    Adding some cards to groups page

--- src/pages/GroupsPage.jsx
+++ src/pages/GroupsPage.jsx
@@ -1,7 +1,19 @@
+import Card from '../components/Card';
+import Button from '../components/Button';
+
 function GroupsPage() {
   return (
-    <div className="bg-slate-200 border rounded">
-      <p className="h-40 w-full flex items-center justify-center font-medium">Groups Page Works!</p>
+    <div className="flex flex-col">
+      <div className="flex justify-end my-4">
+        <Button text="New Group" action={() => console.log('click on new group')} size="md" />
+      </div>
+      <div className="flex gap-2 flex-wrap md:flex-none">
+        <Card className="w-full sm:w-[calc(50%-4px)] lg:w-[calc(100%/3-8px)]" color="#4E80A5" />
+        <Card className="w-full sm:w-[calc(50%-4px)] lg:w-[calc(100%/3-8px)]" color="#6EAC64" />
+        <Card className="w-full sm:w-[calc(50%-4px)] lg:w-[calc(100%/3-8px)]" color="#A65293" />
+        <Card className="w-full sm:w-[calc(50%-4px)] lg:w-[calc(100%/3-8px)]" color="#4E80A5" />
+        <Card className="w-full sm:w-[calc(50%-4px)] lg:w-[calc(100%/3-8px)]" color="#A65293" />
+      </div>
     </div>
   );
 }
