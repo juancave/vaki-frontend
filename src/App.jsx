c6a22a32ff1a466273b93cf70c5b617c809c0df2
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-04-03 14:57:27 -0500

    Creating navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,17 @@
-import { Link, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import GroupsPage from './pages/GroupsPage';
 import FriendsPage from './pages/FriendPage';
 import ExpensesPage from './pages/ExpensesPage';
 import HomePage from './pages/HomePage';
 import MyAccountPage from './pages/MyAccountPage';
+import Navbar from './components/Navbar';
 
-import logo from './assets/logo.svg';
 import wwcLogo from './assets/wwc-logo.png';
 
 function App() {
   return (
     <div className="flex flex-col h-screen justify-between">
-      <header className="flex justify-between items-center bg-vaki-primary text-white  p-4">
-        <div>
-          <Link className="font-medium hover:underline" to="/">
-            <div className="flex items-center gap-2">
-              <img src={logo} alt="Vaki Logo" />
-              <span>Vaki</span>
-            </div>
-          </Link>
-        </div>
-        <div className="flex gap-8">
-          <Link className="font-medium hover:underline" to="/friends">
-            Friends
-          </Link>
-          <Link className="font-medium hover:underline" to="/expenses">
-            Expenses
-          </Link>
-          <Link className="font-medium hover:underline" to="/groups">
-            Groups
-          </Link>
-        </div>
-        <Link className="font-medium hover:underline" to="/my-account">
-          My Account
-        </Link>
-      </header>
+      <Navbar />
       <main className="mt-4 mb-auto px-4">
         <Routes>
           <Route path="/" element={<HomePage />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import logo from '../assets/logo.svg';
+
+function Navbar() {
+  const location = useLocation();
+  const { pathname } = location;
+
+  return (
+    <header className="flex justify-between items-center flex-wrap sm:flex-nowrap bg-vaki-primary text-white p-4 text-xl sm:text-2xl">
+      <div>
+        <Link className="hover:underline" to="/">
+          <div className="flex items-center gap-2">
+            <img src={logo} alt="Vaki Logo" />
+            <span>Vaki</span>
+          </div>
+        </Link>
+      </div>
+      <div className="flex mt-3 sm:mt-0 order-last sm:order-none justify-between sm:justify-normal w-full sm:w-fit gap-8">
+        <NavbarLink pathname={pathname} text="Friends" path="/friends" />
+        <NavbarLink pathname={pathname} text="Expenses" path="/expenses" />
+        <NavbarLink pathname={pathname} text="Groups" path="/groups" />
+      </div>
+      <NavbarLink pathname={pathname} text="My Account" path="/my-account" />
+    </header>
+  );
+}
+
+function NavbarLink({ pathname, path, text }) {
+  return (
+    <Link className="hover:underline relative flex justify-center" to={path}>
+      {text}
+      {pathname === path && <TriangleUp />}
+    </Link>
+  );
+}
+
+NavbarLink.propTypes = {
+  pathname: PropTypes.string,
+  path: PropTypes.string,
+  text: PropTypes.string,
+};
+
+function TriangleUp() {
+  return (
+    <div
+      className="w-0 h-0 absolute bottom-[-16px] sm:bottom-[-18px]
+    border-l-[8px] border-l-transparent
+    border-b-[8px] border-b-white
+    border-r-[8px] border-r-transparent"
+    ></div>
+  );
+}
+
+export default Navbar;
