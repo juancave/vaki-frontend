700fa2e2746ea66c727e23d8130061cca129a765
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-04-03 13:36:44 -0500

    Applying colors to header

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import GroupsPage from './pages/GroupsPage';
 import FriendsPage from './pages/FriendPage';
 import ExpensesPage from './pages/ExpensesPage';
 import HomePage from './pages/HomePage';
+import MyAccountPage from './pages/MyAccountPage';
+
+import logo from './assets/logo.svg';
+import wwcLogo from './assets/wwc-logo.png';
 
 function App() {
   return (
     <div className="flex flex-col h-screen justify-between">
-      <header className="flex justify-between border-b-2 border-slate-200 p-4">
+      <header className="flex justify-between items-center bg-vaki-primary text-white  p-4">
         <div>
           <Link className="font-medium hover:underline" to="/">
-            Vaki APP
+            <div className="flex items-center gap-2">
+              <img src={logo} alt="Vaki Logo" />
+              <span>Vaki</span>
+            </div>
           </Link>
         </div>
         <div className="flex gap-8">
@@ -25,7 +32,9 @@ function App() {
             Groups
           </Link>
         </div>
-        <div>My Account</div>
+        <Link className="font-medium hover:underline" to="/my-account">
+          My Account
+        </Link>
       </header>
       <main className="mt-4 mb-auto px-4">
         <Routes>
@@ -33,9 +42,12 @@ function App() {
           <Route path="/groups" element={<GroupsPage />} />
           <Route path="/friends" element={<FriendsPage />} />
           <Route path="/expenses" element={<ExpensesPage />} />
+          <Route path="/my-account" element={<MyAccountPage />} />
         </Routes>
       </main>
-      <footer className="flex justify-center">2024 WWC Medellín</footer>
+      <footer className="flex justify-center">
+        <img src={wwcLogo} alt="WWC Logo" />
+      </footer>
     </div>
   );
 }
